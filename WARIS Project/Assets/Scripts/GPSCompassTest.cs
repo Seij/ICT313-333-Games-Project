65178fd5bf1fefe0dc448ff28322c0fa27afc9a3
Seij <welsleylui_ackty.bit48@hotmail.com>
2014-10-19 20:32:14 +0800

    GPSCompass1

---
+++ WARIS Project/Assets/Scripts/GPSCompassTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GPSCompassTest : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Input.location.Start ();
+		Input.compass.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(0, -Input.compass.trueHeading, 0);
+	}
+}
